aeaf2ae53b4ba60b9ae7b4a7d0318114f35bca5b
kajuga <kajuga@mail.ru>
2020-03-02 22:46:44 +0300

    init commit

---
+++ src/main/java/afedorov/entities/Address.java
@@ -0,0 +1,8 @@
+package afedorov.entities;
+
+public class Address {
+
+    private Client client;
+    private String country;
+    //TODO
+}
---
+++ src/main/java/afedorov/entities/Client.java
@@ -0,0 +1,7 @@
+package afedorov.entities;
+
+public class Client {
+
+    private String name;
+    // TODO other fields
+}
